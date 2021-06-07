511af7fd729724992a6947c2c3ca829adba8fea5
Askhab Musaev <42musaev@gmail.com>
2021-06-07 16:26:24 +0300

    added filters, ordering

--- store/views.py
+++ store/views.py
@@ -2,10 +2,13 @@ from rest_framework import viewsets
 from store.models import Book
 from store.serializer import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filter_fields = ['price']
+    search_fields = ['name', 'author']
+    ordering_fields = ['price', 'author']
