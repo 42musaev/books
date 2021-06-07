b9568f21057ef2244ba95e8932b225d249ef5fd4
Askhab Musaev <42musaev@gmail.com>
2021-06-07 16:14:32 +0300

    added filters in BookView

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from store.models import Book
 from store.serializer import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['price']
